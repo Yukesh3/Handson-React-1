e03efcef088715c79e84ea2d59b9f13cab756bd8
Yukesh3 <yukesh.fsd@gmail.com>
2022-05-12 18:23:57 +0530

    Toggle

--- src/component/App.js
+++ src/component/App.js
@@ -9,19 +9,19 @@ export default function App() {
 
 
   const [b1, setBox1] = useState(false);
-  const [b2, setBox2] = useState(false)
+  const [b2, setBox2] = useState(false);
 
 
   return (
     <>
       <h1>Styling using Functional and Class component</h1>
       
-      <button className='btn1' onClick={() => setBox1(true)} >Functional Component</button>
+      <button className='btn1' onClick={() => setBox1(!b1)} >Functional Component</button>
 
-      <button className='btn2' onClick={() => setBox2(true)}>Class Component</button>
+      <button className='btn2' onClick={() => setBox2(!b2)}>Class Component</button>
 
-      {b1 && <Functional />}
-      {b2 && <ClassComponent />}
+      {b1? <Functional /> :null}
+      {b2?  <ClassComponent />:null}
 
     </>
   )
