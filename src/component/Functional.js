dcfc948b45ff9f6681ae463914098a92e68d1d30
Yukesh3 <94380589+Yukesh3@users.noreply.github.com>
2022-05-21 18:44:33 +0530

    Done

--- src/component/Functional.js
+++ src/component/Functional.js
@@ -1,19 +1,96 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import '../index.css'
 
 const Functional = () => {
 
+  const [name, setName] = useState({
+    username: "",
+    department: "",
+    rating: "",
+  });
+
+  const { username, department, rating } = name;
+
+  const [record, setRecords] = useState([]);
+  const [next, setNext] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+
+
+    const newRecord = { ...name }
+    setRecords([...record, newRecord]);
+
+    setName({ username: "", department: "", rating: "" })
+
+    setNext(true)
+  }
+
+  const handleChange = (user) => {
+    setName({ ...name, [user.target.name]: [user.target.value] })
+    console.log(name)
+  }
+  const back = () => {
+    setNext(false)
+  }
+
   return (
+
     <>
 
+      {next ? null :
+        <div >
 
-      <div id='open1' className='box_1'>
+          <h1>EMPLOYEE FEEDBACK FORM</h1>
 
-        <h1 style={{ fontSize: '38px' }}>This is created using Functional Component</h1>
-        <p>This is done using external css</p>
-        <p style={{ color: 'blue' }}>This is done using inline css</p>
 
-      </div>
+          <form className='form_1' onSubmit={handleSubmit}>
+
+            <label for='username'>Name :</label><br />
+            <input type='text' id='username' name='username' value={username} onChange={handleChange} required autoComplete='off'  /> <br /><br />
+
+
+            <label for='department'>Department  :</label><br />
+            <input type='text' id='department' name='department' value={department} onChange={handleChange} required autoComplete='off' /><br /><br />
+
+
+            <label for='rating'>Rating  :</label><br />
+            <input type="number" id='rating' name='rating' value={rating} onChange={handleChange} required autoComplete='off' /> <br /><br />
+
+
+            <button type='submit'>Submit</button>
+
+          </form>
+
+        </div>}
+
+      {next ?
+        <div >
+          <h1>EMPLOYEE FEEDBACK DATA</h1>
+          <div id='container'>
+            {record.map((current) => {
+              const { username, department, rating } = current;
+              return (
+
+                <div className='item'>
+                  Name : {username} <span>|</span> Department : {department}  <span>|</span>  Rating : {rating}
+                </div>
+
+              )
+            }
+
+
+            )}
+
+
+          </div>
+
+          <div>
+            {next ? <input id='submit' style={{ marginTop: '15px', width: "180px" }} onClick={back} type="button" value="Go Back" /> : null}
+          </div>
+
+        </div> : null}
 
     </>
 
@@ -22,3 +99,5 @@ const Functional = () => {
 }
 
 export default Functional
+
+
